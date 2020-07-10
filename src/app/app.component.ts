a6233a70ca10bf4805fc807cf4c5326fb2456eb7
Yaswanth Bonda <yaswanthbonda@gmail.com>
2020-07-10 00:03:23 -0400

    second commit of reactive forms assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +14,34 @@ export class AppComponent implements OnInit{
 
   ngOnInit(){
     this.mySignupForm = new FormGroup({
-      'projectName' : new FormControl(null, [Validators.required]),
+      'projectName' : new FormControl(null, [Validators.required, this.forbiddenProjectName.bind(this)]),
       'email' : new FormControl(null, [Validators.required, Validators.email]),
       'projectStatus' : new FormControl(null)
     });
   }
 
+  onSubmit(){
+    console.log(this.mySignupForm);
+    this.mySignupForm.reset();
+  }
 
+  forbiddenProjectName(control: FormControl): {[s: string]: boolean}{
+    if(control.value === 'Test'){
+      return {'projNameIsForbidden': true}
+    }
+    return null;
+  }
+
+  forbiddenProjectNameAsync(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new  Promise<any>( (resolve, reject)=> {
+      setTimeout( ()=>
+        {
+          if(control.value === 'Test'){
+            resolve({'projNameIsForbidden': true});
+          }else resolve(null);
+        }, 2000 
+      );
+    });
+    return promise;
+  }
 }
